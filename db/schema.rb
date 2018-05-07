3c5e43fabe0f36f44b73842e47646725a2a7df38
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-07 11:54:05 -0400

    added img_url attribute on items

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2018_05_07_141151) do
   create_table "items", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.string "img_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
