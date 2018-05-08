3e080c39ad6c5f8cba031c7e3e8610eeb878ff34
github username <github email address>
2018-05-08 17:30:15 -0400

    login, signup, create

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_05_08_171847) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "user_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
