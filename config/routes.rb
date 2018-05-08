56309017f86433456030bf4705cff8b9f88198c1
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-08 10:07:24 -0400

    cleaned out old models

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :glist_items
-  resources :blist_items
+  resources :list_items
   resources :scoreboards
     root  'scoreboards#index'
-  resources :glists
+  resources :lists
   resources :items
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/models/list_item_test.rb
+++ test/models/list_item_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class BlistTest < ActiveSupport::TestCase
+class ListItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
