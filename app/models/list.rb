5011c6fb956abbeb50fe4bdd9c3a620b3195b4bd
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-08 11:19:40 -0400

    nothing?

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,10 @@ class List < ApplicationRecord
   belongs_to :user
   has_many :list_items
   has_many :items, through: :list_items
+
+  def get_items_in_order
+    self.items.map do |item|
+
+    end
+  end
 end
