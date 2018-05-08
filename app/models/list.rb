0f94bb3810d10163bbffed3dddeee98ad387beb1
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-08 15:03:09 -0400

    Added #items_in_order List method

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,14 +4,20 @@ class List < ApplicationRecord
   has_many :items, through: :list_items
 
   def items_in_order
-# get all list_items and select the ones where list_id = self.id
+# get all list_items and select the ones where list_id == self.id
 # order this array by rank
 #then map this over items
     a = ListItem.all.select do |list_item|
-      list_item.list_id = self.id
+      list_item.list_id == self.id
     end
+
     a.sort_by do |item|
       item.rank
+    end
+
+    a.map do |item|
+      the_item = Item.find(item.item_id)
+      [the_item, item.reason]
     end
   end
 end
