42cf3bcc7f58a8476a4dd3a1c3772900b8345d8a
github username <github email address>
2018-05-08 12:02:37 -0400

    dummy params

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_one :list
-  validates :name, presence: true, uniqueness: true
+  # validates :name, presence: true, uniqueness: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  
+
   # devise :database_authenticatable, :registerable,
   #        :recoverable, :rememberable, :trackable, :validatable
 
