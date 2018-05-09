5b76378e48e9c66bf8305931fa42c9f05d86f37f
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-09 17:28:58 -0400

    working on edit form for already completed lists

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,6 +1,21 @@
 class ListItemsController < ApplicationController
   def create
-     @list_item = ListItem.create(item_id: params[:list_item][:item_id], reason: params[:list_item][:reason], list_id: params[:list_item][:list_id], rank: params[:list_item][:rank])
+    @list_item = ListItem.all.find do |litem|
+      litem.list_id == list_id: params[:list_item][:list_id] && litem.rank == rank: params[:list_item][:rank]
+    end
+
+
+    # ListItem.find_by(list_id: params[:list_item][list_id], rank: params[:list_item][:rank])
+
+    byebug
+
+    if !@list_item
+
+      @list_item = ListItem.create(item_id: params[:list_item][:item_id], reason: params[:list_item][:reason], list_id: params[:list_item][:list_id], rank: params[:list_item][:rank])
+
+    else
+
+    end
 
      redirect_to edit_user_path(current_user)
   end
