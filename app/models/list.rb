89eb35c08c8ffcb8d359f4a59a75dfa84eb1106c
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-09 09:26:03 -0400

    merged up with shawn

--- app/models/list.rb
+++ app/models/list.rb
@@ -4,9 +4,9 @@ class List < ApplicationRecord
   has_many :items, through: :list_items
 
   def items_in_order
-# get all list_items and select the ones where list_id == self.id
-# order this array by rank
-#then map this over items
+    # get all list_items and select the ones where list_id == self.id
+    # order this array by rank
+    #then map this over items
     a = ListItem.all.select do |list_item|
       list_item.list_id == self.id
     end
