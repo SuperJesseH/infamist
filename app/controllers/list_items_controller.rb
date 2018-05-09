c3e0e988eb5b558275dde34edb0af234718aa83a
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-09 18:06:18 -0400

    Got most of the list / user edit functionality working

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,19 +1,21 @@
 class ListItemsController < ApplicationController
   def create
     @list_item = ListItem.all.find do |litem|
-      litem.list_id == list_id: params[:list_item][:list_id] && litem.rank == rank: params[:list_item][:rank]
+      litem.list_id == params[:list_item][:list_id].to_i && litem.rank == params[:list_item][:rank].to_i
     end
 
 
     # ListItem.find_by(list_id: params[:list_item][list_id], rank: params[:list_item][:rank])
 
-    byebug
+    # byebug
 
     if !@list_item
 
       @list_item = ListItem.create(item_id: params[:list_item][:item_id], reason: params[:list_item][:reason], list_id: params[:list_item][:list_id], rank: params[:list_item][:rank])
 
     else
+
+      @list_item.update(item_id: params[:list_item][:item_id], reason: params[:list_item][:reason], list_id: params[:list_item][:list_id], rank: params[:list_item][:rank])
 
     end
 
