69b8b6f89f0e93202dfb1812714d61409d8a0ff3
SuperJesseH <therealjhorwitz@gmail.com>
2018-05-10 09:52:21 -0400

    homepage Scoreboard raking

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,9 +1,5 @@
 class ScoreboardsController < ApplicationController
   def index
-
-  end
-
-  def new
     #MOVE TO INDEX WHEN READY
     # REFACTOR WITH "WHERE" - CURRENT USE OF LOOPS VERY SLOW FOR SQL
     @scoreboard = Item.all.map do |item|
@@ -11,12 +7,16 @@ class ScoreboardsController < ApplicationController
         list_item.rank
       end
         if item.list_items.size > 0
-          avgrank = ((11-(rank.to_f / item.list_items.size)) * item.list_items.size) / List.all.size
+          avgrank = ((11.1-(rank.to_f / item.list_items.size)) * item.list_items.size) / List.all.size
           [item, avgrank]
         else
           [item, 0]
         end
     end
     @scoreboard = @scoreboard.sort_by {|i| i[1]}.reverse
+  end
+
+  def new
+
    end
 end
