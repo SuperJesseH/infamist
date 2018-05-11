a811a509980c677c1467c5894f4713527695e687
github username <github email address>
2018-05-11 03:36:29 -0400

    design

--- app/controllers/scoreboards_controller.rb
+++ app/controllers/scoreboards_controller.rb
@@ -1,5 +1,6 @@
 class ScoreboardsController < ApplicationController
   def index
+    @items = Item.all
     @scoreboard = Item.all.map do |item|
       rank = item.list_items.sum do |list_item|
         list_item.rank
