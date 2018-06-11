e61dc00d6df5c9e789931cb731017ce2e9c5d03f
github username <github email address>
2018-06-11 15:43:32 -0400

    css changes

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-
 module Infamist
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -16,5 +15,6 @@ module Infamist
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
   end
 end
